70410a26dc2c568c6c32eeafe93709695cb5b9f6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-29 20:51:06 +0000

    Deploy ssistoza/pokemon-viewer to github.com/ssistoza/pokemon-viewer.git:gh-pages

---
+++ precache-manifest.2c4b172078700374b368dba0ca0fd9e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "795b0aec17b3f8b17aef",
+    "url": "/static/js/main.3f3c7dbe.chunk.js"
+  },
+  {
+    "revision": "13cecfba574090f581e8",
+    "url": "/static/js/2.df14a024.chunk.js"
+  },
+  {
+    "revision": "386b3e0e5417afcad8170ff52f415294",
+    "url": "/index.html"
+  }
+];
